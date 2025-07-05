83bf2c2b9cbc7c65914f72392da2acae6c8c2b0b
tempiltin <temurbekshukurov0707@gmail.com>
2025-07-06 00:00:00 +0500

    Commit on 2025-07-06T00:00:00

--- pascal/code_2025-07-06_82.pas
+++ pascal/code_2025-07-06_82.pas
@@ -1,1 +1,1 @@
-program Factorial; var n, f, i: integer; begin n := 5; f := 1; for i := 1 to n do f := f * i; writeln(f); end.
+program PrintNumbers; var i: integer; begin for i := 1 to 10 do writeln(i); end.
